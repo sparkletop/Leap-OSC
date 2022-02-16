490382e5d9c4b8b277844cca6ce286850db4cdf5
Anders Eskildsen <22001464+sparkletop@users.noreply.github.com>
2022-02-16 15:28:05 +0100

    add basic OSC transmission functionality

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 const osc = require('osc');
 const Leap = require('leapjs');
 
+const config = require('./config.json');
+
 console.log('First steps');
+
+const port = new osc.UDPPort({
+  remoteAddress: config.targetIP,
+  remotePort: config.targetPort,
+  localAddress: config.localIP,
+  localPort: config.localPort,
+});
+
+port.open();
+
+port.on('ready', () => {
+  port.send({
+    address: '/test',
+    args: [
+      {
+        type: 'i',
+        value: 50,
+      },
+    ],
+  });
+});
+
+port.on('error', function (error) {
+  console.log('An error occurred: ', error.message);
+});
